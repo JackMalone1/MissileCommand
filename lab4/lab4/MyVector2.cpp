380579dc35c10c6e34409b8c6f6b97f7c3568478
Jack Malone <c00236428@itcarlow.ie>
2018-11-25 10:04:41 +0000

    added vector2f library from lab3

---
+++ lab4/lab4/MyVector2.cpp
@@ -0,0 +1,111 @@
+#include "MyVector2.h"
+#include <math.h>
+#include "Game.h"
+/*
+author Jack Malone
+C00236428
+estimated time taken: 3 hours
+each of the functions perform different vector maths calculations
+each of the functions are tested in the mainline using the debugger and test cases I worked out on paper beforehand
+the program calculates the vector length, the vector length squared, the vector cross product, the vector dot product,
+the angle between 2 vectors, a vector being rotated through an angle, vector projection for 2 vectors, scalar rejection,
+vector scalar projection and the unit vector for a given vector
+known bugs: none
+*/
+
+/// <summary>
+/// get length of vector using sqrt of the sum of the squares
+/// </summary>
+/// <param name="t_vector">input vector</param>
+/// <returns>length</returns>
+float vectorLength(sf::Vector2f t_vector)
+{
+	float sumOfSquares = (t_vector.x * t_vector.x ) + (t_vector.y * t_vector.y);
+	const float length = sqrt(sumOfSquares);
+	return length;
+}
+//get the length squared by gettting the sum of the squares
+//gets a vector and returns the length squared
+float vectorLengthSquared(sf::Vector2f t_vector)
+{
+	float lengthSquared = (t_vector.x * t_vector.x) + (t_vector.y * t_vector.y);
+	return lengthSquared;
+}
+//gets the vector cross product of 2 vectors using the formula (t_vectorA.x * t_VectorB.y) - (t_vectorA.y * t_VectorB.x)
+//takes 2 vectors and returns the vector cross product
+float vectorCorssProduct(sf::Vector2f t_vectorA, sf::Vector2f t_VectorB)
+{
+	float vectorCrossProduct = (t_vectorA.x * t_VectorB.y) - (t_vectorA.y * t_VectorB.x);
+	return vectorCrossProduct;
+}
+//get the dot product by multiplying the two x values and adding the result to the two y values multiplied together
+//takes two vectors as input
+float vectorDotProduct(sf::Vector2f t_vectorA, sf::Vector2f t_VectorB)
+{
+	float dotProduct = (t_vectorA.x * t_VectorB.x) + (t_vectorA.y * t_VectorB.y);
+	return dotProduct;
+}
+//finds the angle between two vectors by getting the cos inverse of the dot product divided by the product of the magnitudes
+//takes two vectors as input and returns an angle in radians
+float vectorAngleBetween(sf::Vector2f t_vectorA, sf::Vector2f t_VectorB)
+{
+	float dotProduct = vectorDotProduct(t_vectorA, t_VectorB);
+	float lengthVectorA = vectorLength(t_vectorA);
+	float lengthVectorB = vectorLength(t_VectorB);
+	float lengthsMultiplied = lengthVectorA * lengthVectorB;
+	float dotProductDividedByMagnitude = dotProduct / lengthsMultiplied;
+	float angleBetween = acos(dotProductDividedByMagnitude);
+	return angleBetween;
+}
+//rotates a vector through a certain amount of degrees and gives the resulting vector.
+//finds new x value with the formula (cosOfAngle * t_vector.x) - (sinOfAngle * t_vector.y)
+//finds the new y value with the formula (sinOfAngle * t_vector.x) + (cosOfAngle * t_vector.y)
+//takes a vector and an angle in radians and returns a vector
+sf::Vector2f vectorRotateBy(sf::Vector2f t_vector, float t_angleRadians)
+{
+	float cosOfAngle = cosf(t_angleRadians);
+	float sinOfAngle = cosf(t_angleRadians);
+	float xOfNewVector = (cosOfAngle * t_vector.x) - (sinOfAngle * t_vector.y);
+	float yOfNewVector = (sinOfAngle * t_vector.x) + (cosOfAngle * t_vector.y);
+	sf::Vector2f vectorRotated(xOfNewVector, yOfNewVector);
+	return vectorRotated;
+}
+
+//gets the vector projection of two vectors by multiplying the dot product divided by the magnitude by the first vector divided
+//by the magnitude of the other vector
+//takes two vectors as input and returns the projected vector
+sf::Vector2f vectorProjection(sf::Vector2f t_vector, sf::Vector2f t_onto)
+{
+	float dotProduct = vectorDotProduct(t_vector, t_onto);
+	float length = vectorLength(t_onto);
+	sf::Vector2f projectedVector((dotProduct/length) * (t_vector / length));
+	return projectedVector;
+}
+//vector that is perpendicular to the first vector
+//resultant of this vector and the other vector is the vector projection
+//found using the formula t_vector - projectedVector
+//takes two vectors as inputs and returns the rejected vector
+sf::Vector2f vectorRejection(sf::Vector2f t_vector, sf::Vector2f t_onto)
+{
+	sf::Vector2f projectedVector(vectorProjection(t_vector, t_onto));
+	sf::Vector2f rejectedVector(t_vector - projectedVector);
+	
+	return rejectedVector;
+}
+//gets the scalar projection by dividing the dot product by the magnitude squared of one of the vectors
+//takes two vectors as input and returns a scalar
+float vectorScalarProjection(sf::Vector2f t_vector, sf::Vector2f t_onto)
+{
+	float dotProduct = vectorDotProduct(t_vector, t_onto);
+	float lengthSquared = vectorLengthSquared(t_vector);
+	float scalarProjection = dotProduct / lengthSquared;
+	return scalarProjection;
+}
+//finds the unit vector for a given vector by dividing the vector by its magnitude
+//takes a vector as an input and returns a vector
+sf::Vector2f vectorUnitVector(sf::Vector2f t_vector)
+{
+	float length = vectorLength(t_vector);
+	sf::Vector2f unitVector(t_vector.x / length, t_vector.y / length);
+	return unitVector;
+}
